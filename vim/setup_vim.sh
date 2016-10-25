61b4a7a883e5ca5ace0d78cfc864dea0c28bf9a0
Mat Pataki <matpataki@gmail.com>
2016-10-24 22:17:33 -0400

    fix vim setup

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -1,6 +1,6 @@
 function setup_vim(){
   if [ -e $HOME/.vimrc ]; then
-    print_with_color $YELLOW "$HOME/.bash_profile already exists. Do you want to override it? (yes/no)"
+    print_with_color $YELLOW "$HOME/.vimrc already exists. Do you want to override it? (yes/no)"
     read yn
     case $yn in
       yes ) check_and_link_file `pwd`/vim/vim_config $HOME/.vimrc;;
