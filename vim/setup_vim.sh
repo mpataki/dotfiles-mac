854b13bb1a35d66ad557ac83fcea0a04743153f7
Mat Pataki <matpataki@gmail.com>
2017-02-02 22:35:59 -0500

    update vimrc

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -3,11 +3,11 @@ function setup_vim(){
     print_with_color $YELLOW "$HOME/.vimrc already exists. Do you want to override it? (yes/no)"
     read yn
     case $yn in
-      yes ) check_and_link_file `pwd`/vim/vim_config $HOME/.vimrc;;
+      yes ) check_and_link_file `pwd`/vim/vimrc $HOME/.vimrc;;
       * ) print_with_color $GREEN 'skipping...';;
     esac
   else
-    check_and_link_file `pwd`/vim/vim_config $HOME/.vimrc
+    check_and_link_file `pwd`/vim/vimrc $HOME/.vimrc
   fi
 }
 
