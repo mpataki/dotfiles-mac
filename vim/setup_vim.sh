ca1f472141eefc516f0a0e306e5ac6684fbc8b5e
Mat Pataki <matpataki@gmail.com>
2017-09-18 21:10:18 -0400

    ack vim

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -8,6 +8,7 @@ function setup_vim(){
 
   git_clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
   git_clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/airline.vim
+  git clone https://github.com/mileszs/ack.vim.git ~/.vim/bundle/ack.vim
 }
 
 print_with_color $YELLOW 'Setup Vim? (yes/no)'
