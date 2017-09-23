9b13dec1d6fc74f0dbf3b686dffa4f5816d4a83e
Mat Pataki <matpataki@gmail.com>
2017-09-22 23:39:59 -0400

    vim colors

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -2,13 +2,10 @@ function setup_vim(){
   homebrew_install vim vim
   check_and_link_file `pwd`/vim/vimrc $HOME/.vimrc
 
-  if ! [ -e ~/.vim ]; then
-    mkdir ~/.vim
-  fi
-
-  git_clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
-  git_clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/airline.vim
-  git clone https://github.com/mileszs/ack.vim.git ~/.vim/bundle/ack.vim
+  git_clone https://github.com/ctrlpvim/ctrlp.vim.git $HOME/.vim/bundle/ctrlp.vim
+  git_clone https://github.com/vim-airline/vim-airline.git $HOME/.vim/bundle/airline.vim
+  git clone https://github.com/mileszs/ack.vim.git $HOME/.vim/bundle/ack.vim
+  git clone https://github.com/chr4/jellygrass.vim.git  $HOME/.vim/bundle/jellygrass.vim
 }
 
 print_with_color $YELLOW 'Setup Vim? (yes/no)'
