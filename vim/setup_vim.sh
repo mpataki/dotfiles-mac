d47b0f5e71a3f5b6bde56d278a77d44ea26507e8
Mat Pataki <matpataki@gmail.com>
2017-09-23 00:48:13 -0400

    vim colors

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -5,7 +5,8 @@ function setup_vim(){
   git_clone https://github.com/ctrlpvim/ctrlp.vim.git $HOME/.vim/bundle/ctrlp.vim
   git_clone https://github.com/vim-airline/vim-airline.git $HOME/.vim/bundle/airline.vim
   git clone https://github.com/mileszs/ack.vim.git $HOME/.vim/bundle/ack.vim
-  git clone https://github.com/chr4/jellygrass.vim.git  $HOME/.vim/bundle/jellygrass.vim
+
+  cp -r vim/colors $HOME/.vim/
 }
 
 print_with_color $YELLOW 'Setup Vim? (yes/no)'
