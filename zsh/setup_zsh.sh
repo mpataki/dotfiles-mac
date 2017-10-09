6256aaaca923fa769d6975e1b96429f97e0d29e3
Mat Pataki <matpataki@gmail.com>
2017-10-09 01:12:46 -0400

    setup zsh and bash config files to source aliases and env vars from the same place

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,13 +1,30 @@
+function write_zshrc() {
+  print_with_color $GREEN "writing $HOME/.zshrc"
+
+  echo ". `pwd`/zsh/zshrc" > $HOME/.zshrc
+}
+
 function setup_zsh(){
   homebrew_install 'zsh' 'zsh'
   homebrew_install 'zsh-completions' 'zsh-completions'
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-  check_and_link_file `pwd`/zsh/zshrc $HOME/.zshrc
-
   # hub completions
   mkdir -p ~/.zsh/completions
   cp /usr/local/share/zsh/site-functions/_hub ~/.zsh/completions/_hub
+
+  git_clone 'https://github.com/geometry-zsh/geometry.git' "$HOME/.oh-my-zsh/custom/themes/geometry"
+
+  if [ -e $HOME/.zshrc ]; then
+    print_with_color $YELLOW "$HOME/.zshrc already exists. Do you want to override it? (yes/no)"
+    read yn
+    case $yn in
+      yes ) write_zshrc;;
+      * ) print_with_color $GREEN 'skipping...';;
+    esac
+  else
+    write_zshrc
+  fi
 
   print_with_color $YELLOW 'Do you want to set zsh as the default shell? (yes/no)'
   read yn
