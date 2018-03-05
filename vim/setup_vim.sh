4390a2857f642f1ad42f9b4fe66d333290dc399e
Mat Pataki <matpataki@gmail.com>
2018-03-04 23:13:46 -0500

    vim plugs vis Plug, not manial

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -1,10 +1,6 @@
 function setup_vim(){
   homebrew_install vim vim
   check_and_link_file `pwd`/vim/vimrc $HOME/.vimrc
-
-  git_clone https://github.com/ctrlpvim/ctrlp.vim.git $HOME/.vim/bundle/ctrlp.vim
-  git_clone https://github.com/vim-airline/vim-airline.git $HOME/.vim/bundle/airline.vim
-
   check_and_link_file `pwd`/vim/colors $HOME/.vim/colors
 }
 
